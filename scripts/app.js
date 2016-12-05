20ef057ee167069ca6565e39b70300503f840ea7
Jef DeWitt <jefdewitt@gmail.com>
2016-12-04 22:11:28 -0500

    adds additional service & json file

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,50 @@
 
 angular.module('angularApp', [])
 
-.controller('firstCtrl', function($scope) {
+.controller('firstCtrl', function($scope, dataService) {
+
     $scope.learningNgChange = function() {
         console.log('A change has occurred!');
     };
 
-    $scope.fields = [
-        {'name': 'title'},
-        {'name': 'time'}
-    ]
+    $scope.helloConsole = dataService.helloConsole;
+
+    dataService.getFields(function(response) {
+        console.log(response.data);
+        $scope.fields = response.data;
+    });
+
+    $scope.deleteField = function(field, $index) {
+        dataService.deleteField(field);
+        $scope.fields.splice($index, 1);
+    };
+
+    // $scope.saveField = function(field, $index) {
+    //     dataService.saveField(field);
+    //     $scope.fields.push($index, 1);
+    // };
+
+})
+
+.service('dataService', function($http) {
+
+    this.helloConsole = function() {
+        console.log('This is the hello console service!');
+    };
+
+    this.getFields = function(callback) {
+        $http.get('mock/fields.json')
+        .then(callback)
+    };
+
+    this.deleteField = function(field) {
+        console.log("The " + field.name + "field has been deleted.")
+
+    };
+
+    this.saveField = function(field) {
+        console.log("The " + field.name + "field has been saved.")
+
+    };
 
 });
