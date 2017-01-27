efa70f18a7a0897b76f6b21ad972db426a24a0e3
Jef DeWitt <jefdewitt@gmail.com>
2017-01-27 09:34:43 -0500

    adds time to calendar date cell

--- scripts/controllers/detail.js
+++ scripts/controllers/detail.js
@@ -14,16 +14,15 @@ angular.module('angularApp')
 
     $scope.timeObject = $scope.Detail.timeRepo;
 
-    function buildCal(m, y, cM, cH, cDW, cD, brdr){
-        var twelveMonths=['January','February','March','April','May','June','July','August','September','October','November','December'];
-        var lastDayOfMonths=[31,0,31,30,31,30,31,31,30,31,30,31];
+    var twelveMonths=['January','February','March','April','May','June','July','August','September','October','November','December'];
 
+    function buildCal(m, y, cM, cH, cDW, cD, brdr){
+
+        var lastDayOfMonths=[31,0,31,30,31,30,31,31,30,31,30,31];
         var firstDayOfYear = new Date(y, m-1, 1); //DD replaced line to fix date bug when current day is 31st
-        console.log('firstDayOfYear ' + firstDayOfYear);
         firstDayOfYear.dayOne=firstDayOfYear.getDay()+1; //DD replaced line to fix date bug when current day is 31st
-        console.log('firstDayOfYear.dayOne ' + firstDayOfYear.dayOne);
         var todaydate=new Date() //DD added
-        var scanfortoday=(y==todaydate.getFullYear() && m==todaydate.getMonth()+1)? todaydate.getDate() : 0 //DD added
+        var scanForToday=(y==todaydate.getFullYear() && m==todaydate.getMonth()+1)? todaydate.getDate() : 0 //DD added
 
         lastDayOfMonths[1]=(((firstDayOfYear.getFullYear()%100!=0)&&(firstDayOfYear.getFullYear()%4==0))||(firstDayOfYear.getFullYear()%400==0))?29:28;
         var table='<div class="'+cM+'"><table class="'+cM+'" cols="7" cellpadding="0" border="'+brdr+'" cellspacing="0"><tr align="center">';
@@ -33,24 +32,19 @@ angular.module('angularApp')
         table+='</tr><tr align="center">';
         for(i=1;i<=42;i++){
             var x=((i-firstDayOfYear.dayOne>=0)&&(i-firstDayOfYear.dayOne<lastDayOfMonths[m-1]))? i-firstDayOfYear.dayOne+1 : '&nbsp;';
-            if (x==scanfortoday) //DD added
+            if (x==scanForToday) //DD added
             x='<span id="today">'+x+'</span>' //DD added
 
-
-            var displayMonth = twelveMonths[m-1];
+            var displayMonth = m;
             if (displayMonth < 10) { displayMonth = '0' + displayMonth; }
             var displayDay = x;
             if (displayDay < 10) { displayDay = '0' + displayDay; }
 
-
-            table+='<td id=" '+y+' - '+displayMonth+' - '+displayDay+' " class="'+cD+'">'+x+'</td>';
+            table+='<td id="'+y+'-'+displayMonth+'-'+displayDay+'" class="'+cD+'">'+x+'</td>';
             if(((i)%7==0)&&(i<36))table+='</tr><tr align="center">';
         }
         return table+='</tr></table></div>';
     }
-
-    var themonths=['January','February','March','April','May','June',
-    'July','August','September','October','November','December']
 
     var todaydate=new Date()
     var curmonth=todaydate.getMonth()+1 //get current month (1-12)
@@ -70,16 +64,66 @@ angular.module('angularApp')
         }
     }
 
-
     $scope.listOfOptions = [];
 
     var i;
     for (i=0; i<12; i++) {//display option for 12 months of the year
-        var opt = themonths[i] + curyear;
+        var opt = twelveMonths[i] + curyear;
 
         $scope.listOfOptions.push(opt);
     }
 
     document.getElementById("calendar-space").innerHTML=buildCal(curmonth, curyear, "main", "month", "daysofweek", "days", 0);
+
+    // var iDArray = Array.prototype.slice.call(document.querySelectorAll("td[id^='20']"));
+    //
+    // console.log(iDArray);
+
+    // var dateId = document.getElementsByTagName('td').id;
+    // console.log(dateId);
+    //
+    // var elemid = document.getElementById("td[id^='20']").id;
+    // console.log(elemid);
+
+    console.log('$scope.Detail.timeRepo ' + $scope.Detail.timeRepo);
+
+    angular.forEach($scope.Detail.timeRepo, function(index) {
+        console.log(index.timeStamp);
+        console.log('index.minutes ' + index.minutes);
+        var dataCell = document.getElementById(index.timeStamp);
+        console.log('dataCell ' + dataCell);
+        var dataCellId = dataCell.id;
+        console.log('dataCellId ' + dataCellId);
+
+        if( dataCell )
+          {
+              var para = document.createElement("span");
+              var node = document.createTextNode(index.minutes);
+              para.appendChild(node);
+              dataCell.appendChild(para);
+            //   dataCell.append(index.minutes);
+              console.log('we have a winner');
+          }
+
+
+        // var dateId = document.getElementsByTagName('td').id;
+        // console.log(dateId);
+        // if (index.timeStamp)
+        //
+        // angular.forEach('iDArray', function(index2) {
+        //     console.log('index2 ' + index2);
+        //     // if timeStamp and id match
+        //     if (index1.timeStamp === index2.id) {
+        //         console.log('index2.id ' + index2.id);
+        //         console.log('index2 ' + index2);
+        //         console.log('bingo motherfuckers');
+        //         // append the index.minutes to the td with that id
+        //         var dateMatch = document.querySelector(index2);
+        //         dateMatch.innerHTML(index1.timeStamp);
+        //     }
+        // })
+    })
+
+
 
 });
